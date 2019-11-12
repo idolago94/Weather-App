c56376c8688cadda57d6cc1a10e3a0aec44c9c24
idolago94 <idolago94@gmail.com>
2019-11-12 19:45:18 +0200

    api

--- app.js
+++ app.js
@@ -9,22 +9,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || 'localhost:';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 3000;
-
-var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
- 
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
