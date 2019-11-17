98e5155d314d369198e0e67aa7cb4cadab3f3298
idolago94 <idolago94@gmail.com>
2019-11-17 19:42:46 +0200

    app key

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/app-key', function(req, res, next) {
+  res.send(process.env.API_KEY);
+});
+
 module.exports = router;
