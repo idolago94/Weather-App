afc18f99baf25f6ec6d807e443245be5056eb555
idolago94 <idolago94@gmail.com>
2019-11-18 11:39:24 +0200

    fix refresh

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/public/index.html'));
+  res.sendFile(path.resolve(__dirname + '/public/index.html'));
 })
 
 // app.use('/', indexRouter);
